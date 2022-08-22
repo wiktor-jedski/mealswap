bcfc2a68236c8a51233ad48d413eb1872eecc600
wiktor-jedski <jedrzejewskiwiktor@gmail.com>
2022-08-22 14:04:03 +0200

    added Bootstrap4 to app, css/html cleanup

--- app.py
+++ app.py
@@ -1,10 +1,61 @@
-from flask import Flask, render_template
+import os
 
+from flask import Flask, render_template, abort
+from flask_bootstrap import Bootstrap4
+from werkzeug.security import generate_password_hash, check_password_hash
+from flask_sqlalchemy import SQLAlchemy
+from sqlalchemy.orm import relationship
+from flask_login import UserMixin, login_user, LoginManager, login_required, current_user, logout_user
+from forms import RegisterForm, LoginForm
+from functools import wraps
+import mealbrain
+
+# app config
 app = Flask(__name__)
+Bootstrap4(app)
+app.config['SECRET_KEY'] = os.environ.get('SECRET_KEY')
+login_manager = LoginManager()
+login_manager.init_app(app)
+
+# connect to database
+app.config['SQLALCHEMY_DATABASE_URI'] = os.environ.get('DATABASE_URL', 'sqlite:///blog.db')
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+db = SQLAlchemy(app)
+
+
+# database tables config
+class User(UserMixin, db.Model):
+    __tablename__ = 'app_users'
+    id = db.Column(db.Integer, primary_key=True)
+    email = db.Column(db.String(250), nullable=False, unique=True)
+    password = db.Column(db.String(250), nullable=False)
+    name = db.Column(db.String(250), nullable=False)
+
+
+db.create_all()
+
+
+def admin_required(f):
+    @wraps(f)
+    def decorated_f(*args, **kwargs):
+        if current_user.id != 1:
+            return abort(403)
+        return f(*args, **kwargs)
+    return decorated_f
 
 
 @app.route("/")
 def homepage():
+    """Takes the login status.
+    Renders the homepage.
+    Homepage looks differently, depending on the login status:
+    - logged in: calendar view for the current user and search
+    - logged out: search/call to action"""
+    return render_template('/home.html')
+
+
+@app.route("/login")
+def login():
     """Takes the login status.
     Renders the homepage.
     Homepage looks differently, depending on the login status:
